ef853cbc3728051574cba645d2f526a4b5f4ab8d
Gabo <gab_org@hotmail.com>
2019-11-08 10:46:42 -0600

    modificacione spara ios

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 importScripts('js/sw-utils.js');
 
 //2)
-const STATIC_CACHE = 'static_v2';
+const STATIC_CACHE = 'static_v3';
 const DYNAMIC_CACHE = 'dynamic_v1';
 const INMUTABLE_CACHE = 'inmutable_v1';
 
